7a7d858ad3de90850c7b3c9e105bda21c43fd069
carolinaparanhs <carolinaparanhos2004@gmail.com>
2025-12-10 22:21:39 -0300

    Atualiza script do banco

--- ScriptSistemaDuvidas.sql
+++ ScriptSistemaDuvidas.sql
@@ -1,34 +1,15 @@
 
-CREATE DATABASE IF NOT EXISTS sistema_duvidas
-  CHARACTER SET utf8mb4
-  COLLATE utf8mb4_unicode_ci;
+
 
 USE sistema_duvidas;
 
-CREATE TABLE aluno (
-    id_aluno INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    nome VARCHAR(150) NOT NULL,
-    email VARCHAR(150) UNIQUE NOT NULL,
-    senha VARCHAR(100) NOT NULL
-);
+ALTER TABLE duvida
+ADD COLUMN id_professor INT NULL AFTER id_aluno;
 
-CREATE TABLE professor (
-    id_professor INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    nome VARCHAR(150) NOT NULL,
-    email VARCHAR(150) UNIQUE NOT NULL,
-    senha VARCHAR(100) NOT NULL
-); 
+ALTER TABLE duvida
+ADD CONSTRAINT fk_duvida_professor
+    FOREIGN KEY (id_professor)
+    REFERENCES professor(id_professor)
+    ON DELETE SET NULL
+    ON UPDATE CASCADE;
 
-CREATE TABLE duvida (
-    id_duvida INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    id_aluno INT NOT NULL,
-    titulo VARCHAR(200) NOT NULL,
-    descricao TEXT NOT NULL,
-    prioridade ENUM('Alta','Media','Baixa') NOT NULL,
-    status_atendimento ENUM('Aguardando','Em Atendimento','Resolvido') 
-        NOT NULL DEFAULT 'Aguardando',
-    data_criacao TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-    data_resolucao TIMESTAMP NULL,
-    CONSTRAINT fk_duvida_aluno 
-        FOREIGN KEY (id_aluno) REFERENCES aluno (id_aluno)
-); 
